1925c82af681092e19cbddd56ecd4257a7fbe4a1
aprox13 <ifkbhit@gmail.com>
2020-11-12 01:01:14 +0300

    smo

--- svm/svm_test.py
+++ svm/svm_test.py
@@ -4,8 +4,10 @@ from sklearn.model_selection import GridSearchCV
 from tqdm.contrib.concurrent import process_map as pm
 
 from svm.smv import *
+from svm.smo import SMO
 from utils.data_set import DataSet
-from utils.methods import log_action
+from utils.methods import *
+from utils.plots import *
 
 FILE_MASK = "data/{0}.csv"
 
@@ -24,7 +26,7 @@ def pm_predict(f, data: np.ndarray, name):
     return pm(f, data, desc=f'{name}', max_workers=CHOOSE_BEST_THREADS, chunksize=25000)
 
 
-def draw(clf: SVM, ds: DataSet, step):
+def draw(clf, ds: DataSet, step):
     X = ds.get_X()
     y = ds.get_y()
     x_min, y_min = np.amin(X, 0)
@@ -35,6 +37,7 @@ def draw(clf: SVM, ds: DataSet, step):
     mesh_dots = np.c_[xx.ravel(), yy.ravel()]
 
     predict_z = np.array(pm_predict(clf.predict_single, mesh_dots, name='predict')).reshape(xx.shape)
+    # predict_z = np.array(np.apply_along_axis(clf.predict_single, 1, mesh_dots)).reshape(xx.shape)
 
     x0, y0 = X[y == -1].T
     x1, y1 = X[y == 1].T
@@ -61,21 +64,33 @@ def flatten(a):
         return flatten(a[0]) + flatten(a[1:])
     return [a]
 
-KERNELS = flatten([
-    Linear(),
-    [RBF(beta) for beta in [0.001, 0.01, 0.1, 1.0, 2.0, 3.0, 4.0, 5.0]],
-    [Poly(d, 0) for d in range(2, 6)],
-])
 
-GRID = {
+BASIC_GRID = {
     "C": [0.05, 0.1, 0.5, 1.0, 5.0, 10.0, 50.0, 100.0, 1000.0],
-    "kernel": KERNELS,
-    'max_iter': [100, 500, 1000]
+    'max_iters': [100, 500, 1000]
 }
 
 
+def basic_with(d: dict):
+    res = {}
+    for k, v in BASIC_GRID.items():
+        res[k] = v
+    for k, v in d.items():
+        res[k] = v
+    return res
+
+
+LINEAR_GRID = basic_with({'kernel': ['linear']})
+RBF_GRID = basic_with({'kernel': ['rbf'], 'gamma': [0.001, 0.01, 0.1, 1.0, 2.0, 3.0, 4.0, 5.0]})
+
+GRID = [
+    LINEAR_GRID,
+    # RBF_GRID
+]
+
+
 def choose_best(ds: DataSet):
-    gs = GridSearchCV(estimator=SVM(),
+    gs = GridSearchCV(estimator=SMO(),
                       param_grid=GRID,
                       cv=4,
                       scoring='accuracy',
@@ -83,18 +98,67 @@ def choose_best(ds: DataSet):
                       n_jobs=-1)
 
     gs.fit(ds.get_X(), ds.get_y())
-
+    draw_metrics(gs.cv_results_)
     print(f'Got best score {gs.best_score_} with params {gs.best_params_}')
     return gs.best_params_
+
+
+def kernel_to_s(d):
+    kernel = d['kernel']
+    if kernel == 'linear':
+        return 'linear'
+    if kernel == 'rbf':
+        return f'rbf, γ={d["gamma"]}'
+    if kernel == 'poly':
+        return f'poly, d={d["degree"]}'
+    return 'unknown'
+
+
+def draw_metrics(cv_res):
+    for iter in BASIC_GRID["max_iters"]:
+        data = {}
+
+        def append_metric(key, v):
+            if key not in data:
+                data[key] = []
+            data[key].append(v)
+
+        for C in BASIC_GRID['C']:
+            for kernel in map(lambda d: d['kernel'], GRID):
+                search = {
+                    'kernel': kernel,
+                    'C': C,
+                    'max_iters': iter
+                }
+                idx = index_where(dict_contains(search), cv_res['params'])
+                name = kernel_to_s(cv_res['params'][idx])
+                append_metric(name, cv_res['mean_test_score'][idx])
+        metric_plot(
+            data,
+            BASIC_GRID['C'],
+            f"Test score with max iters {iter}",
+            x_label='C',
+            with_text='false'
+        )
 
 
 def process(name):
     ds = log_action("Reading", lambda: read_dataset(FILE_MASK.format(name)), with_start_msg=True)
     svm_best_params = log_action("Choosing best svm", lambda: choose_best(ds), with_start_msg=True)
-
-    svm_best = SVM()
+    svm_best = SMO()
     svm_best.set_params(**svm_best_params)
 
     print(f"Got {svm_best}")
     log_action("trainig", lambda: svm_best.fit(ds.get_X(), ds.get_y()), with_start_msg=True)
+    y_pred = svm_best.predict(ds.get_X())
+
+    from sklearn.metrics import accuracy_score
+    sc = accuracy_score(ds.get_y(), y_pred.T)
+
+    print(f'Train score {sc}')
+
     log_action("drawing", lambda: draw(svm_best, ds, step=0.01))
+
+
+if __name__ == '__main__':
+    process('geyser')
