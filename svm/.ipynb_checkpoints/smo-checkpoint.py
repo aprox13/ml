e16c3407b7abcb895b01386510d94794c24b7594
aprox13 <ifkbhit@gmail.com>
2020-11-12 10:39:37 +0300

    save

---
+++ svm/.ipynb_checkpoints/smo-checkpoint.py
@@ -0,0 +1,144 @@
+import numpy as np
+from sklearn.base import BaseEstimator
+import scipy.spatial.distance as dist
+
+
+def random(not_eq, max_ex, min_inc=0):
+    itr = 0
+    while itr < 1000:
+        j = np.random.randint(min_inc, max_ex - 1)
+        if j != not_eq:
+            return j
+        itr += 1
+    raise RuntimeError('Couldn\'t get random')
+
+
+def bound(value, min_v, max_v):
+    return max(min_v, min(max_v, value))
+
+
+def join_funcs(f1, f2):
+    def inner(*args, **kwargs):
+        return f1(f2(*args, **kwargs))
+
+    return inner
+
+
+class SMO(BaseEstimator):
+
+    def _kernel_linear(self, x, y):
+        if x.shape == y.shape:
+            return np.dot(x, y)
+        return np.dot(x, y.T)
+
+    def _kernel_poly(self, x, y):
+        if x.shape == y.shape:
+            return np.dot(x, y) ** self.degree + 1
+        return np.dot(x, y.T) ** self.degree + 1
+
+    def _kernel_rbf(self, x, y):
+        tx = np.atleast_2d(x)
+        ty = np.atleast_2d(y)
+        return np.exp(-self.gamma * dist.cdist(tx, ty) ** 2).flatten()
+
+    def _kernel(self, x, y):
+        return SMO.__getattribute__(self, f'_kernel_{self.kernel}')(x, y)
+
+    def __init__(self, eps=0.000001, kernel='linear', gamma=0, degree=0, C=1, max_iters=1000):
+        self.kernel = kernel
+        self.gamma = gamma
+        self.C = C
+        self.eps = eps
+        self.max_iters = max_iters
+
+        self.a = None
+        self.b = 0
+        self.X = np.array([])
+        self.y = np.array([])
+        self._kernel_matrix = np.array([])
+        self.support_indices = np.array([])
+        self.X_for_predict = np.array([])
+        self.a_dot_y = np.array([])
+        self.degree = degree
+
+    def fit(self, X, y):
+        self.X = X
+        self.y = y
+        C = self.C
+        N = len(X)
+        self._kernel_matrix = np.array(
+            [self._kernel(X[i], X[j])
+             for i in range(N)
+             for j in range(N)
+             ]
+        ).reshape((N, N))
+
+        self.a = np.zeros(N)
+        for _ in range(self.max_iters):
+            alpha_prev = np.copy(self.a)
+
+            for j in range(N):
+                i = random(not_eq=j, max_ex=N)
+
+                K_ii = self._kernel_matrix[i, i]
+                K_ij = self._kernel_matrix[i, j]
+                K_jj = self._kernel_matrix[j, j]
+
+                y_i = self.y[i]
+                y_j = self.y[j]
+
+                a_i = self.a[i]
+                a_j = self.a[j]
+
+                eta = 2.0 * K_ij - K_ii - K_jj
+                if eta >= 0:
+                    continue
+
+                if y_i == y_j:
+                    L = max(0, a_i + a_j - C)
+                    H = min(C, a_i + a_j)
+                else:
+                    L = max(0, a_j - a_i)
+                    H = min(C, C - a_i + a_j)
+
+                E_i = self._predict_learn(i) - self.y[i]
+                E_j = self._predict_learn(j) - self.y[j]
+
+                self.a[j] = bound(
+                    a_j - (y_j * (E_i - E_j)) / eta,
+                    max_v=H,
+                    min_v=L)
+
+                self.a[i] = a_i + y_i * y_j * (a_j - self.a[j])
+
+                da_i = self.a[i] - a_i
+                da_j = self.a[j] - a_j
+
+                b1 = self.b - E_i - y_i * da_i * K_ii - y_j * da_j * K_ij
+                b2 = self.b - E_j - y_j * da_j * K_jj - y_i * da_i * K_ij
+
+                if 0 < self.a[i] < C:
+                    self.b = b1
+                elif 0 < self.a[j] < C:
+                    self.b = b2
+                else:
+                    self.b = (b1 + b2) / 2.0
+
+            if np.linalg.norm(self.a - alpha_prev) < self.eps:
+                break
+
+        self.support_indices = np.where(self.a > 0)[0]
+
+        self.X_for_predict = self.X[self.support_indices]
+        self.a_dot_y = (self.a[self.support_indices] * self.y[self.support_indices]).T
+
+    def predict(self, X):
+        return np.apply_along_axis(self.predict_single, 1, X)
+
+    def _predict_learn(self, i):
+        k_v = self._kernel_matrix[i, :]  # X @ X[i]
+        return np.dot((self.a * self.y), k_v) + self.b
+
+    def predict_single(self, X):
+        k_v = self._kernel(self.X_for_predict, X)
+        return np.sign(np.dot(self.a_dot_y, k_v) + self.b)
---
+++ svm/.ipynb_checkpoints/svm_test-checkpoint.py
@@ -0,0 +1,167 @@
+from sklearn.model_selection import GridSearchCV
+from tqdm.contrib.concurrent import process_map as pm
+
+from svm.smo import SMO
+from utils.data_set import DataSet
+from utils.methods import *
+from utils.plots import *
+
+FILE_MASK = "data/{0}.csv"
+
+CHOOSE_BEST_THREADS = 12
+
+
+def read_dataset(filename) -> DataSet:
+    data = pd.read_csv(filename)
+    X = data.values[:, :-1]
+    tmp_y = data.values[:, -1]
+    y = np.vectorize(lambda t: 1 if t == 'P' else -1)(tmp_y)
+    return DataSet(X, y)
+
+
+def pm_predict(f, data: np.ndarray, name):
+    return pm(f, data, desc=f'{name}', max_workers=CHOOSE_BEST_THREADS, chunksize=25000)
+
+
+def draw(clf, ds: DataSet, step):
+    X = ds.get_X()
+    y = ds.get_y()
+    x_min, y_min = np.amin(X, 0)
+    x_max, y_max = np.amax(X, 0)
+    xx, yy = np.meshgrid(np.arange(x_min, x_max, step),
+                         np.arange(y_min, y_max, step))
+
+    grid = np.c_[xx.ravel(), yy.ravel()]
+
+    predict_z = np.array(pm_predict(clf.predict_single, grid, name='predict')).reshape(xx.shape)
+
+    x0, y0 = X[y == -1].T
+    x1, y1 = X[y == 1].T
+
+    X_sup = X[clf.support_indices]
+    x_sup, y_sup = X_sup.T
+
+    def plot(_predict_z):
+        plt.figure(figsize=(10, 10))
+        plt.pcolormesh(xx, yy, _predict_z, cmap=plt.get_cmap('seismic'), shading='auto')
+        plt.scatter(x0, y0, color='red', s=100)
+        plt.scatter(x1, y1, color='blue', s=100)
+
+        plt.scatter(x_sup, y_sup, color='white', marker='x', s=60)
+        plt.show()
+
+    plot(predict_z)
+
+
+def flatten(a):
+    if isinstance(a, list):
+        if len(a) == 0:
+            return []
+        return flatten(a[0]) + flatten(a[1:])
+    return [a]
+
+
+BASIC_GRID = {
+    "C": [0.1, 0.5, 1.0, 5.0, 10.0, 50.0, 100.0, 1000.0],
+    'max_iters': [500, 1000]
+}
+
+
+def basic_with(d: dict):
+    res = {}
+    for k, v in BASIC_GRID.items():
+        res[k] = v
+    for k, v in d.items():
+        res[k] = v
+    return res
+
+
+LINEAR_GRID = basic_with({'kernel': ['linear']})
+RBF_GRID = basic_with({'kernel': ['rbf'], 'gamma': [0.001, 0.01, 0.1, 1.0, 2.0, 3.0, 4.0, 5.0]})
+POLY_GRID = basic_with({'kernel': ['poly'], 'degree': [2.0, 3.0, 4.0, 5.0]})
+
+GRID = [
+    LINEAR_GRID,
+    RBF_GRID,
+    POLY_GRID
+]
+
+
+def choose_best(ds: DataSet):
+    gs = GridSearchCV(estimator=SMO(),
+                      param_grid=GRID,
+                      cv=4,
+                      scoring='accuracy',
+                      verbose=1,
+                      n_jobs=-1)
+
+    gs.fit(ds.get_X(), ds.get_y())
+    draw_metrics(gs.cv_results_)
+    print(f'Got best score {gs.best_score_} with params {gs.best_params_}')
+    return gs.best_params_
+
+
+def kernel_to_s(d):
+    kernel = d['kernel']
+    if kernel == 'linear':
+        return 'linear'
+    if kernel == 'rbf':
+        return f'rbf, γ={d["gamma"]}'
+    if kernel == 'poly':
+        return f'poly, d={d["degree"]}'
+    return 'unknown'
+
+
+metrics_params = {
+    'linear': ['kernel'],
+    'rbf': ['gamma']
+}
+
+
+def draw_metrics(cv_res):
+    for iter in BASIC_GRID["max_iters"]:
+        data = {}
+
+        def append_metric(key, v):
+            if key not in data:
+                data[key] = []
+            data[key].append(v)
+
+        for C in BASIC_GRID['C']:
+            for kernel in map(lambda d: d['kernel'][0], GRID):
+                idxs = indices_where(has_all(C=C, max_iters=iter, kernel=kernel), cv_res['params'])
+                for idx in idxs:
+                    name = kernel_to_s(cv_res['params'][idx])
+                    append_metric(name, cv_res['mean_test_score'][idx])
+        metric_plot(
+            data,
+            BASIC_GRID['C'],
+            f"Test score with max iters {iter}",
+            x_label='C',
+            with_text=False,
+            default_color=True,
+            fit_x=True,
+            n_col=4
+        )
+
+
+def process(name):
+    ds = log_action("Reading", lambda: read_dataset(FILE_MASK.format(name)), with_start_msg=True)
+    svm_best_params = log_action("Choosing best svm", lambda: choose_best(ds), with_start_msg=True)
+    svm_best = SMO()
+    svm_best.set_params(**svm_best_params)
+
+    print(f"Got {svm_best}")
+    log_action("trainig", lambda: svm_best.fit(ds.get_X(), ds.get_y()), with_start_msg=True)
+    y_pred = svm_best.predict(ds.get_X())
+
+    from sklearn.metrics import accuracy_score
+    sc = accuracy_score(ds.get_y(), y_pred.T)
+
+    print(f'Train score {sc}')
+
+    log_action("drawing", lambda: draw(svm_best, ds, step=0.01))
+
+
+if __name__ == '__main__':
+    process('geyser')
