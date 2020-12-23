de87815cb129683c5e4bd2b504c829bc6450c771
aprox13 <ifkbhit@gmail.com>
2020-12-24 01:24:38 +0300

    save cnn

--- cnn/mnist.py
+++ cnn/mnist.py
@@ -1,10 +1,15 @@
 # %%
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import seaborn as sn
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
+from sklearn.metrics import accuracy_score, confusion_matrix
 from torch.utils.data import DataLoader
 from torchvision import transforms
-from torchvision.datasets import MNIST
+from torchvision.datasets import MNIST, FashionMNIST
 from tqdm.notebook import trange, tqdm
 
 
@@ -38,6 +43,8 @@ class MnistNet(nn.Module):
 
 
 # %%
+
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
 
 
 def dataset_loader(dataset_class, root, train, transform, download, shuffle, batch_size):
@@ -77,20 +84,18 @@ metrics = {
 }
 
 
-def train_test_model(dataset_class,
-                     criterion,
-                     optimizer_cls,
-                     learning_rate=1e-3,
-                     batch_size=32,
-                     model=None,
-                     num_epochs=20,
-                     ):
+def train_model(dataset_class,
+                criterion,
+                optimizer_cls,
+                learning_rate=1e-3,
+                batch_size=32,
+                model=None,
+                num_epochs=20,
+                ):
     if model is None:
         model = MnistNet()
 
     optimizer = optimizer_cls(model.parameters(), lr=learning_rate)
-
-    transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.1307,), (0.3081,))])
 
     train_loader = get_train_dataloader(dataset_class, transform, batch_size)
 
@@ -114,17 +119,14 @@ def train_test_model(dataset_class,
         i = -1
         for images, labels in tqdm(train_loader):
             i += 1
-            # Прямой запуск
             outputs = model(images)
             loss = criterion(outputs, labels)
             loss_list.append(loss.item())
 
-            # Обратное распространение и оптимизатор
             optimizer.zero_grad()
             loss.backward()
             optimizer.step()
 
-            # Отслеживание точности
             total = labels.size(0)
             _, predicted = torch.max(outputs.data, 1)
             correct = (predicted == labels).sum().item()
@@ -134,30 +136,85 @@ def train_test_model(dataset_class,
             train_metric['loss'].append(loss.item())
             train_metric['step'].append(epoch + 1 + (i / total_step))
 
-            # if (i + 1) % 100 == 0:
-            #     print('Epoch [{}/{}], Step [{}/{}], Loss: {:.4f}, Accuracy: {:.2f}%'
-            #           .format(epoch + 1, num_epochs, i + 1, total_step, loss.item(),
-            #                   (correct / total) * 100))
-
         plots(train_metric)
 
-
-    model.eval()
-    test_loader = get_test_dataloader(dataset_class, transform, batch_size)
-    with torch.no_grad():
-        correct = 0
-        total = 0
-        for images, labels in test_loader:
-            outputs = model(images)
-            _, predicted = torch.max(outputs.data, 1)
-            total += labels.size(0)
-            correct += (predicted == labels).sum().item()
-
-        print('Test Accuracy of the model on the 10000 test images: {} %'.format((correct / total) * 100))
-
-    # Сохраняем модель и строим график
     torch.save(model.state_dict(), 'conv_net_model.ckpt')
+    return model
 
 
 # %%
-train_test_model(MNIST, criterion=nn.CrossEntropyLoss(), optimizer_cls=torch.optim.Adam)
+
+def show_img_matrix(images_matrix):
+    nrow = len(images_matrix)
+    ncol = len(images_matrix[0])
+    f, axarr = plt.subplots(
+        nrow, ncol,
+        gridspec_kw=dict(wspace=0.1, hspace=0.1,
+                         top=1. - 0.5 / (nrow + 1), bottom=0.5 / (nrow + 1),
+                         left=0.5 / (ncol + 1), right=1 - 0.5 / (ncol + 1)),
+        figsize=(ncol + 1, nrow + 1),
+        sharey='row', sharex='col',  # optionally
+    )
+
+    for ax in axarr.ravel():
+        ax.set_xticklabels([])
+        ax.set_yticklabels([])
+        ax.set_axis_off()
+
+    for i in range(len(images_matrix)):
+        for j in range(len(images_matrix[i])):
+            image = images_matrix[i][j]
+            axarr[i, j].imshow(image)
+    plt.show()
+
+
+def test_model(dataset_class,
+               model=None,
+               batch_size=32):
+    model.eval()
+    test_loader = get_test_dataloader(dataset_class, transform, batch_size)
+    img_arr = np.zeros((10, 10, 28, 28, 1))
+    y_true_pred = [[], []]
+    with torch.no_grad():
+        for images, labels in tqdm(test_loader):
+            outputs = model(images)
+            _, predicted = torch.max(outputs.data, 1)
+
+            y_true_pred[0].extend(labels)
+            y_true_pred[1].extend(predicted)
+
+            for idx, (true, pred) in enumerate(zip(labels, predicted)):
+                img_arr[true, pred] = images[idx].reshape((28, 28, 1))
+
+        acc = accuracy_score(y_true=y_true_pred[0], y_pred=y_true_pred[1])
+        size = len(y_true_pred[0])
+        print(
+            'Test accuracy {:.2f}% - {}/{}'.format(acc * 100, acc * size, size)
+        )
+        conf_matrix = confusion_matrix(y_true=y_true_pred[0], y_pred=y_true_pred[1])
+        index, columns = conf_matrix.shape
+        df_cm = pd.DataFrame(conf_matrix, index=range(index),
+                             columns=range(columns))
+        plt.figure(figsize=(10, 7))
+        vmax = np.unique(y_true_pred[1], return_counts=True)[1].max()
+        sn.heatmap(df_cm, annot=True, cmap='coolwarm', vmin=0, vmax=vmax, fmt='.4g')
+        show_img_matrix(img_arr)
+
+
+# %%
+def trained_model(file_path):
+    model = MnistNet()
+    model.load_state_dict(torch.load(file_path))
+    return model
+
+
+# %%
+model = train_model(MNIST, criterion=nn.CrossEntropyLoss(), optimizer_cls=torch.optim.Adam)
+
+# model = trained_model('/Users/ifkbhit/itmo/ml-git/data/conv_net_model.ckpt')
+
+test_model(MNIST, model)
+
+# %%
+model = train_model(FashionMNIST, criterion=nn.CrossEntropyLoss(), optimizer_cls=torch.optim.Adam)
+test_model(FashionMNIST, model)
