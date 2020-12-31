8b5bab2da279408906c4b5824177e77df5964675
aprox13 <ifkbhit@gmail.com>
2020-12-31 12:53:36 +0300

    try this

--- bayes/bayes.py
+++ bayes/bayes.py
@@ -1,14 +1,11 @@
 import os
 import re
-from functools import partial
-from multiprocessing.spawn import freeze_support
 
 import matplotlib.pyplot as plt
 import numpy as np
 from nltk import ngrams
 from sklearn.metrics import confusion_matrix
 from tqdm import trange
-from tqdm.contrib.concurrent import process_map as pm
 
 LEGIT: int = 1
 SPAM: int = 0
@@ -46,6 +43,7 @@ def calc_ngrams(X, n):
 
 
 def calc_X_test(keys, X_test, n):
+    print('calc_X_test shape', X_test)
     ng = dict.fromkeys(keys, 0)
     for i in range(0, len(X_test) - n):
         key = tuple(X_test[i:i + n])
@@ -98,16 +96,14 @@ def predict(y_train, X_test, _lambda, prob):
     return 1
 
 
-def cross_validation(l_max, X, y, alpha=1e-3):
+def cross_validation(l_max, alpha=1e-3):
+    X, y = load_dataset()
     l = (1, l_max)
-    acc = []
-    true_pred, false_pred, false_legit = 0, 0, 0
-    k0, k1 = 0, 0
+    CM = np.zeros((2, 2))
+
     for i in trange(len(X), desc='Main loop'):
         ngrams, X_train = calc_ngrams(np.append(X[:i], X[i + 1:]), N)
         y_train = np.append((y[:i]), (y[i + 1:]))
-
-        y_control = y[i]
 
         likelihood_y = calc_likelihood(X_train, y_train, alpha)
 
@@ -116,25 +112,11 @@ def cross_validation(l_max, X, y, alpha=1e-3):
             X_test = calc_X_test(ngrams, X[i][j], N)
             y_test.append(predict(y_train, X_test, l, likelihood_y))
 
-        CM = confusion_matrix(y_true=y_control, y_pred=y_test)
-
-        TN = CM[0][0]
-        FN = CM[1][0]
-        TP = CM[1][1]
-        FP = CM[0][1]
-
-        true_pred += TP
-        false_pred += FP
-        false_legit += FN
-
-        acc.append((TN + TP) / np.sum(CM))
-
-        k0 += np.sum(y_control == SPAM)
-        k1 += np.sum(y_control == LEGIT)
-    return np.mean(acc), false_legit, true_pred / k1, false_pred / k0
+        CM += confusion_matrix(y_true=y[i], y_pred=y_test)
+    return CM
 
 
-def solve():
+def load_dataset():
     X = []
     y = []
     dirs = os.listdir("./data/")
@@ -145,19 +127,37 @@ def solve():
 
     X = np.array(X)
     y = np.array(y)
+    return X, y
 
+
+def solve():
     lambdaa = [i for i in range(1, int(1e308), int(1e308 / 10))]
     ox = []
     oy = []
+    acc = []
 
-    res = map(partial(cross_validation, X=X, y=y), lambdaa)
+    res = map(cross_validation, lambdaa)
 
-    for (acc, false_legit, true_pred, false_pred) in res:
-        ox.append(true_pred)
-        oy.append(false_pred)
+    for CM in res:
+        # CM[true][pred]
+        TN = CM[SPAM][SPAM]
+        FN = CM[LEGIT][SPAM]
+        TP = CM[LEGIT][LEGIT]
+        FP = CM[SPAM][LEGIT]
+
+        total_P = TP + FN
+        total_N = FP + TN
+        acc.append((TN + TP) / CM.sum())
+
+        ox.append(TP / total_P)
+        oy.append(FP / total_N)
 
     plt.plot(ox, oy)
     plt.title("roc")
+    plt.show()
+
+    plt.plot(lambdaa, acc)
+    plt.title('Accuracy')
     plt.show()
 
 
